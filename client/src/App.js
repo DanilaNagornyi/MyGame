f50528c91ba2ec348d1e95fe91a93d0c494dcda9
Artnagornyi <artnagornyi@gmail.com>
2021-04-02 15:26:55 +0300

    Create design

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Router>
+    {/* // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //   </header>
+    // </div> */}
+        <Navbar />
+        </Router>
   );
 }
 
